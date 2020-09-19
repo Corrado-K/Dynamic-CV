4f859e9ac03464c573657c70c881c3b74c03fdb8
kebemartey@gmail.com <kebemartey@gmail.com>
2020-09-19 13:54:27 +0000

    Added files

---
+++ cv.js
@@ -0,0 +1,12 @@
+
+const names = form.elements["names"].value;
+const address = form.elements["address"].value;
+const phone = form.elements["phone"].value;
+const nationality= form.elements["nationality"].value;
+const email = form.elements["email"].value;
+const url = form.elements["url"].value;
+
+
+
+
+addEventListener("load",addValues)
