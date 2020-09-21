9fe96e624ae45a5fa85452eef04f8f7d367a9e3b
kebemartey@gmail.com <kebemartey@gmail.com>
2020-09-21 16:04:42 +0000

    Modified

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ function getData(){
     let address2 = document.getElementById("address").value;
     let phone2= document.getElementById("phone").value;
     let email2 = document.getElementById("email").value;
+    let gender2 = document.getElementById("gender").value;
 
     localStorage.setItem("v1", name2);
     localStorage.setItem("v2", address2);
     localStorage.setItem("v3", phone2);
     localStorage.setItem("v5", email2);
+    localStorage.setItem("v6",gender2);
 
 }
 
@@ -21,8 +23,48 @@ document.getElementById("namedata").innerHTML=localStorage.getItem("v1");
 document.getElementById("addressdata").innerHTML=localStorage.getItem("v2");
 document.getElementById("phonedata").innerHTML=localStorage.getItem("v3");
 document.getElementById("emaildata").innerHTML=localStorage.getItem("v5");
+document.getElementById("genderdata").innerHTML=localStorage.getItem("v6");
+
+
+const form = document.getElementById("newform");
 
 
 
+function validate(){
 
+    let error_msg ="";
 
+    const fname = form.elements["fnames"].value;
+    const mname = form.elements["fnames"].value;
+    const lname = form.elements["fnames"].value;
+    const address = form.elements["address"].value;
+    const phone = form.elements["phone"].value
+    const email = form.elements["email"].value
+    const gender = form.elements["gender"].value;
+
+    if (fname === null) {
+        // error_msg += "Error, Please enter first name \n";
+        return false;
+    }
+    if (lname === null) {
+        // error_msg += "Error, Please enter last name \n";
+        return false;
+    }
+    if (phone === null) {
+        error_msg += "Error, Please enter phone number \n";
+        return false;
+    }
+    if (address === null) {
+        // error_msg += "Error, Please enter last name \n";
+        return false;
+    }
+    if (email === null) {
+        // error_msg += "Error, Please enter email \n";
+        return false;
+    }
+    if (gender === null) {
+        // error_msg += "Error, Please enter gender\n";
+        return false;
+    }
+
+}
